d29c7d484677b4f686e4a40854e508b34ebba7bc
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-07-19 15:51:41 +0530

    first commit

---
+++ 07-datatypes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NO1=$1
+NO2=$2
+
+
+#addition of two variables
+SUM=(($NO1+$NO2))
+echo "The sum of two variables are ${SUM}"
+#subtraction of two variables
+SUB=(($NO1-$NO2))
+echo "The sub of two variables are ${SUB}"
+#multiplication of two variables
+MUL=(($NO1*$NO2))
+echo "The sub of two variables are ${MUL}"
