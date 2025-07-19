0fe43c0670b3e23a5cced4785ad9134143dc7c2f
vasanth-mvvr <vasanthreddy@example.com>
2025-07-19 15:43:21 +0530

    first commit

---
+++ 06-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Arrays is a datatype which is used to store homogeneous elements
+#Arrays starts with index zero
+#Two ways to get the result that is 1.) $ and 2.) ${}
+
+MOVIES=("RRR" "PUSHPA")
+
+echo "The first movie is : ${MOVIES[0]}"
