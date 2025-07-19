0bcec0b2862dabd12396c94ce70b325cbd96b699
vasanth-mvvr <vasanthreddy@example.com>
2025-07-19 13:12:24 +0530

    Added name

--- 02_variables.sh
+++ 02_variables.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 #The first line is shebang acts as the interpreter
 
-person1 = vasanth
-
-echo "The name is $person1"
+echo "Avenger: Hello Batman,How are you?"
+echo "Batman: Hello Avenger, I am fine. what about you?"
+echo "Avenger: I am fine Batman.How about your work?"
+echo "Batman: work is going good.What about you?"
 
  
