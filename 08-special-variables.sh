1ae751d75e948a619faac78f4e33c95d4c503c4d
vasanth-mvvr <vasanthreddy@example.com>
2025-07-19 16:09:14 +0530

    second commit

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -8,6 +8,6 @@ echo " To get to know about the home directory is : $HOME"
 echo "To get to know about the which user is running the script : $USER "
 echo "Hostname means the sever name: $HOSTNAME"
 echo "Process ID of the current script: $$"
-sleep 60 #foreground
-#sleep 60 & #background 
-#echo "process ID of last background command : $!"
+#sleep 60 #foreground
+sleep 60 & #background 
+echo "process ID of last background command : $!"
