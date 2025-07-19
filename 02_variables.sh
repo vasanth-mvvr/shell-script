09e7719c4df8256b13cca2b915d97446739797e1
vasanth-mvvr <vasanthreddy@example.com>
2025-07-19 13:22:00 +0530

    Added name

--- 02_variables.sh
+++ 02_variables.sh
@@ -5,5 +5,3 @@ echo "Avenger: Hello Batman,How are you?"
 echo "Batman: Hello Avenger, I am fine. what about you?"
 echo "Avenger: I am fine Batman.How about your work?"
 echo "Batman: work is going good.What about you?"
-
- 
