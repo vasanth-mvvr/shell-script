567869b4bb1acc7ab764b852d5494b769eab4c31
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-07-19 13:22:00 +0530

    Added name

---
+++ 03_variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#In the 02_variables.sh the disadvantage is every time we need to add the repeated names they are Avenger and Batman
+#To overcome that we are making use of variables
+
+PERSON1=Avenger
+PERSON2=Batman
+
+echo "$PERSON1: Hello $PERSON2,How are you?"
+echo "$PERSON2: Hello $PERSON1, I am fine. what about you?"
+echo "$PERSON1: I am fine $PERSON2.How about your work?"
+echo "$PERSON2: work is going good.What about you?"
