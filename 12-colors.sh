6311665da6e528342b166f16ba69c15c7b90bd36
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-07-20 07:01:26 +0530

    first commit

---
+++ 12-colors.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#In this file we are making using of colors 
+USERID=$(id -u)
+TIMESTAMP=$(date +%F +%H-%M-%S)     #Here we will get the date and time
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1) #Here we are getting the file name with out extension using the cut command for the first fragment
+LOGFILE=/tmp/$TIMESTAMP-$SCRIP_TNAME.log #Here we have created a logfile which stores in the tmp directory
+
+R=\e[31m
+G=\e[32m
+N=\e[33m
+
+echo "Script started executing : $TIMESTAMP"
+VALIDATE(){
+    if [ $1 -ne 0]
+    then 
+        echo "$2 ... $R Failure $N " #Here we are using color code first we are enabling and then using it  "\e[31m "--> for red color
+    else
+        echo " $2 .... $G Success $N " #Here we are using color code first we are enabling and then using it  "\e[32m "--> for green color
+    else
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "you need to have super user access"
+    exit 1
+else
+    echo "you are super user"
+
+dnf install mysql -y &>>$LOGFILE #Appending the data to the logfile
+VALIDATE $? "Installing mysql"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing git"
---
+++ 14-package-installation.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(+data +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R=/e[31m
+G=/e[32m
+Y=/e[33m
+N=/e[0m
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 $R FAILURE $N "
+    else 
+        echo -e "$2 $G SUCCESS $N"
+}
+if [ $USERID -ne 0]
+then 
+    echo "you are not a super user"
+    exit 1
+else 
+    echo "you are super user"
+fi
+
+
+for i in {$@}
+do 
+    echo "The packages are : $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then 
+        echo -e "Packaged installed .. $Y Skipping $N"
+    else 
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installing package $i"
+    fi
+done
