c41b8c2d4f07120bf43c57435ab8efd084104459
vasanth-mvvr <vasanthreddy@example.com>
2025-07-22 11:25:13 +0530

    updated

--- 20-diskfree.sh
+++ 20-diskfree.sh
@@ -3,8 +3,9 @@
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=75
 
+echo "The disk usage is : $DISK_USAGE "
 while IFS= read -r line
 do
-echo "The disk usage is : $DISK_USAGE "
+
 #awk -F " " '{$print $6}'
 done <<< $DISK_USAGE 
