0f840b36eace50b6186ddef41f3574c20a873c64
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-07-22 11:29:36 +0530

    updated

--- 20-diskfree.sh
+++ 20-diskfree.sh
@@ -6,7 +6,7 @@ DISK_THRESHOLD=75
 echo "The disk usage is : $DISK_USAGE "
 while IFS= read -r line
 do
-DISK_VALUE=awk -F " " '{print $6}' | cut -d "%" -f1
+DISK_VALUE=$(awk -F " " '{print $6}' | cut -d "%" -f1)
 
 #awk -F " " '{$print $6}'
 done <<< $DISK_USAGE 
