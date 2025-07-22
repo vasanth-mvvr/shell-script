92d536fa505b2a5b7a47ef412b8d53ad0631db12
vasanth-mvvr <vasanthreddy@example.com>
2025-07-22 11:51:25 +0530

    updated

--- 20-diskfree.sh
+++ 20-diskfree.sh
@@ -6,16 +6,26 @@ N="\e[30m"
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=75
 
-echo "The disk usage is : $DISK_USAGE "
+#echo "The disk usage is : $DISK_USAGE "
+# while IFS= read -r line
+# do
+# DISK_VALUE=$(awk -F " " '{print $6}' | cut -d "%" -f1)
+# #echo "$DISK_VALUE"
+# if [ $DISK_VALUE -ge $DISK_THRESHOLD ] 
+#     then 
+#         echo -e "$R The data is almost full : $DISK_VALUE $N "
+#     else
+#         echo -e "$G The data is not full : $DISK_VALUE $N "
+# fi
+# #awk -F " " '{$print $6}'
+# done <<< $DISK_USAGE 
+
 while IFS= read -r line
 do
-DISK_VALUE=$(awk -F " " '{print $6}' | cut -d "%" -f1)
-#echo "$DISK_VALUE"
-if [ $DISK_VALUE -ge $DISK_THRESHOLD ] 
+USAGE=$(echo "$line" | awk -F " " '{print $6}' | cut -d "%" -f1)
+FLODER=$(echo "$line" | awk -F " " '{print $N}')
+if [ $USAGE -ge $DISK_THRESHOLD ]
     then 
-        echo -e "$R The data is almost full : $DISK_VALUE $N "
-    else
-        echo -e "$G The data is not full : $DISK_VALUE $N "
+        echo "The folder $FOLDER if greater than the $DISK_THRESHOLD of the current usage : $USAGE"
 fi
-#awk -F " " '{$print $6}'
-done <<< $DISK_USAGE 
+done
